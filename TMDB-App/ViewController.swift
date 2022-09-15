7821c6b8dc278acabea59d083fde0f3c41d7ab4d
Emin Saygı <emin_0001@hotmail.com>
2022-09-15 10:35:02 +0300

    Initial Commit

---
+++ TMDB-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDB-App
+//
+//  Created by Emin Saygı on 15.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
